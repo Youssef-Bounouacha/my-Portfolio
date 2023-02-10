7babf8cebd33492536ea6f2381c6683ae3067af6
nitr0011 <bnyoussef11@gmail.com>
2023-02-10 23:11:18 +0100

    adding packages and adding header

---
+++ my-portfolio/src/Components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+type Props = {}
+
+export default function Header({ }: Props) {
+    return (
+        <header>
+            <div className='flex flex-row items-center'>
+                {/*Socials Icons*/}
+                <SocialIcon url="https://www.linkedin.com/in/youssef-bounouacha-b90981202" fgColor='gray' bgColor='transparent' />
+                <SocialIcon url="https://github.com/Youssef-Bounouacha" fgColor='gray' bgColor='transparent' />
+                <SocialIcon url="https://mail.google.com/mail" fgColor='gray' bgColor='transparent' />
+
+            </div>
+        </header>
+    )
+}
+
---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,32 @@
+import { Inter } from '@next/font/google'
+import Header from '@/Components/Header'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      {/*Header*/}
+      <Header />
+
+      {/*Hero*/}
+
+
+      {/*About*/}
+
+
+      {/*Experience*/}
+
+
+      {/*Skills*/}
+
+
+      {/*Project*/}
+
+
+      {/*Contact*/}
+
+
+    </div>
+  )
+}
