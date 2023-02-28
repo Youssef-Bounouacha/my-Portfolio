5d6c9af024a4cf23559ac785fdf44fb74f2b5f73
nitr0011 <bnyoussef11@gmail.com>
2023-02-28 20:54:14 +0100

    fix issues

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -20,20 +20,18 @@ export default function About({ }: Props) {
                     opacity: 1,
                 }}
                 src='/images/pdp.jpg'
-                className='mt-24 md:mt-0 md:mb-18 flex-shrink-0 w-64 h-64 rounded-full object-top object-cover md:rounded-t-xl md:rounded-b-xl md:h-[320px] md:w-64 '
+                className='mt-24 md:mt-0 md:mb-18 flex-shrink-0 w-48 h-48 rounded-full object-top object-cover md:rounded-t-xl md:rounded-b-xl md:h-[320px] md:w-64 '
             />
             <div className='flex flex-col px-0 md:mb-0 -mt-3 md:mt-16 md:px-5 '>
                 <h4 className='uppercase text-xl md:text-2xl font-semibold md:mb-2'>Let me introduce <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-900'>myself</span></h4>
-                <p className='md:text-base text-xs font-semibold leading-relaxed tracking-wide text-justify'>
+                <p className='md:text-base text-xs font-semibold leading-relaxed tracking-wide text-justify mb-1'>
                     Hi, my name is Youssef Bounouacha and I am a 24-year-old software engineer from Tangier, Morocco.
                     I am passionate about the world of technology and take great pride in my work as a software engineer.
                     My love for video games has also driven my interest in the field and fueled my motivation to continuously
                     improve and stay up-to-date with the latest advancements.<br />
-                    I am a highly motivated and driven individual with a strong work ethic.
                     I possess excellent problem-solving skills and I am able to think critically and creatively.<br />
                     I am always eager to take on new challenges and continuously improve my skills, and I am confident that
                     I can bring a positive and impactful contribution to any organization I work with.
-                    Thank you for considering my profile.
                 </p>
                 <button className='group w-fit px-6 py-3 my-0 mx-auto font-semibold hover:ring-2 ring-red-900 flex items-center uppercase rounded-md bg-gradient-to-r from-red-400 to-red-900'>
                     <a href='pdf/Youssef_BounouachaCV (eng).pdf' download>my resume</a>
