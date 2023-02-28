86e742041eca72926b38fe1c8ee7902042fd715a
nitr0011 <bnyoussef11@gmail.com>
2023-02-28 10:44:20 +0100

    yarn build and re installing lock file

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero({ }: Props) {
             <BackgroundCercles />
             <h1 className='p-5 font-bold text-6xl z-10'>
                 <span className='text-gray'>Hi there!🤗</span><br />
-                <span className='text-gray'>I'm <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-900'>Youssef Bounouacha</span> </span>
+                <span className='text-gray'>I&rsquo;m <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-900'>Youssef Bounouacha</span> </span>
             </h1>
             <h1 className='p-5 font-bold text-6xl z-10'>
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-900'>{text}</span>
--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 
 type Props = {}
 
@@ -6,73 +7,73 @@ export default function Skills({ }: Props) {
     const skill = [
         {
             id: 1,
-            src: 'images/skills/html.png',
+            src: '/images/skills/html.png',
             title: 'HTML',
             style: 'shadow-orange-500',
         },
         {
             id: 2,
-            src: 'images/skills/css.png',
+            src: '/images/skills/css.png',
             title: 'CSS',
             style: 'shadow-blue-500',
         },
         {
             id: 3,
-            src: 'images/skills/js.png',
+            src: '/images/skills/js.png',
             title: 'JavaScript',
             style: 'shadow-yellow-500',
         },
         {
             id: 4,
-            src: 'images/skills/ts.png',
+            src: '/images/skills/ts.png',
             title: 'TypeScript',
             style: 'shadow-blue-700',
         },
         {
             id: 5,
-            src: 'images/skills/react.png',
+            src: '/images/skills/react.png',
             title: 'React',
             style: 'shadow-sky-600',
         },
         {
             id: 6,
-            src: 'images/skills/next.png',
+            src: '/images/skills/next.png',
             title: 'Next',
             style: 'shadow-white',
         },
         {
             id: 7,
-            src: 'images/skills/tailwind.png',
+            src: '/images/skills/tailwind.png',
             title: 'Tailwind',
             style: 'shadow-sky-500',
         },
         {
             id: 8,
-            src: 'images/skills/vite.png',
+            src: '/images/skills/vite.png',
             title: 'VITE',
             style: 'shadow-purple-500',
         },
         {
             id: 9,
-            src: 'images/skills/git.png',
+            src: '/images/skills/git.png',
             title: 'GIT',
             style: 'shadow-red-500',
         },
         {
             id: 10,
-            src: 'images/skills/github.png',
+            src: '/images/skills/github.png',
             title: 'GitHub',
             style: 'shadow-gray-600',
         },
         {
             id: 11,
-            src: 'images/skills/materialui.png',
+            src: '/images/skills/materialui.png',
             title: 'Material UI',
             style: 'shadow-blue-500',
         },
         {
             id: 12,
-            src: 'images/skills/mantineui.png',
+            src: '/images/skills/mantineui.png',
             title: 'Mantine UI',
             style: 'shadow-sky-400',
         },
@@ -84,13 +85,13 @@ export default function Skills({ }: Props) {
                 skills
             </h3>
             <h4 className='absolute top-40 md:top-40 tracking-[3px] z=10 uppercase text-xl md:text-2xl font-semibold md:mb-2'>
-                These are the technologies I've worked with
+                These are the technologies I&rsquo;ve worked with
             </h4>
             <div className='w-full grid grid-cols-4 md:text-lg text-[10px] sm:grid-cols-3 gap-8 text-center py-8 px12 sm:px-0'>
                 {
                     skill.map(({ id, src, title, style }) => (
                         <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                            <img src={src} alt="" className='w-20 mx-auto' />
+                            <Image src={src} alt='skills' width={80} height={80} className='mx-auto' />
                             <p className='mt-4'>{title}</p>
                         </div>
 
